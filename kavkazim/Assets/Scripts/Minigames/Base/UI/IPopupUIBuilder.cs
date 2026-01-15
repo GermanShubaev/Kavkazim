d83301ebcc399e63ca98db3be537a0337f674e3c
Amir Burdini <amir.burdini@sap.com>
2026-01-15 21:24:53 +0200

    clean code #1 - tested and it works

--- kavkazim/Assets/Scripts/Minigames/Base/UI/IPopupUIBuilder.cs
+++ kavkazim/Assets/Scripts/Minigames/Base/UI/IPopupUIBuilder.cs
@@ -2,24 +2,9 @@ using UnityEngine;
 
 namespace Minigames.Base.UI
 {
-    /// <summary>
-    /// Interface for building popup UI for minigames.
-    /// Allows different minigames to have different UI structures
-    /// without modifying the base classes.
-    /// </summary>
     public interface IPopupUIBuilder
     {
-        /// <summary>
-        /// Builds the popup window structure for a minigame.
-        /// </summary>
-        /// <param name="minigame">The minigame instance that needs a popup</param>
-        /// <returns>The created popup window GameObject</returns>
         GameObject BuildPopup(BaseMinigame minigame);
-
-        /// <summary>
-        /// Cleans up any resources created by the builder.
-        /// </summary>
-        /// <param name="popup">The popup window to clean up</param>
         void Cleanup(GameObject popup);
     }
 }
